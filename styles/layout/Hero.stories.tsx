077c8d26f5b46f110915bbdeb80a66e5b44c96b3
Tyler Freeman <tyler.freeman@willowtreeapps.com>
2020-01-16 11:38:31 -0500

    Remove unused imports

--- styles/layout/Hero.stories.tsx
+++ styles/layout/Hero.stories.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
-import { withKnobs, text, boolean, number } from "@storybook/addon-knobs"
 
 import { Hero } from "./Hero"
 
